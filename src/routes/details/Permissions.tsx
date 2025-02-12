f7a2999bf63ce69f26bf3b2128ef214546b571cf
Olexandr L <lunev@aspentechlabs.com>
2025-02-12 07:48:14 +0100

    remove mood permissions

--- src/routes/details/Permissions.tsx
+++ src/routes/details/Permissions.tsx
@@ -16,8 +16,6 @@ import {
 import { useAppDispatch, useAppSelector } from '@/app/hooks';
 import { togglePreferences } from '@/features/preferences/preferences-slice';
 import chromePermissions from '@/lib/chromePermissions.json';
-import SmileSad from '@/components/ui/smile-sad';
-import SmileHappy from '@/components/ui/smile-happy';
 
 export type ChromePermissionsType = {
   [permissionKey: string]: string;
@@ -43,7 +41,6 @@ const Permissions: React.FC<{ extension: Extension }> = ({ extension }) => {
           <h2 className="mb-2 flex gap-1 items-center justify-between">
             <span className="flex gap-1 items-center">
               <LockClosedIcon /> <span>Permissions</span>
-              {permissionProgress >= 20 ? <SmileSad /> : <SmileHappy />}
             </span>
             <TooltipProvider>
               <Tooltip>
