b2be6d557111374dead6ab8d7e4e2c9219affc54
Olexandr L <lunevdev@gmail.com>
2025-02-13 11:59:32 +0100

    feature:enabling/disabling

--- src/service-worker/service-worker.ts
+++ src/service-worker/service-worker.ts
@@ -1,31 +1,36 @@
+import { Extension } from '@/features/extensions/extensions-slice';
 import { matchUrl, storagePersisted } from '@/lib/utils';
 
-const checkAndDisable = async (tabUrl: string) => {
+const checkTab = async () => {
   const extensions = await storagePersisted.get('extensions');
   if (!extensions || !extensions.entities) return;
 
-  for (const extension of extensions.entities) {
-    if (extension.disabledUrls.length > 0) {
-      const shouldBeDisabled = extension.disabledUrls.some(
-        (item: { url: string }) => matchUrl(item.url, tabUrl),
-      );
-      chrome.management.setEnabled(
-        extension.id,
-        shouldBeDisabled ? false : extension.enabled,
-      );
-    }
-  }
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, async (tabs) => {
+    if (!tabs.length || !tabs[0].url) return;
+    const tabUrl = tabs[0].url;
+
+    const updatePromises = extensions.entities.map(
+      async (extension: Extension) => {
+        const shouldBeDisabled = extension.disabledUrls?.some((item) =>
+          matchUrl(item.url, tabUrl),
+        );
+        const shouldBeEnabled = extension.enabledUrls?.some((item) =>
+          matchUrl(item.url, tabUrl),
+        );
+
+        const newState = shouldBeDisabled
+          ? false
+          : shouldBeEnabled
+            ? true
+            : extension.enabled;
+        return chrome.management.setEnabled(extension.id, newState);
+      },
+    );
+
+    await Promise.all(updatePromises);
+  });
 };
 
-chrome.tabs.onUpdated.addListener(async (_tabId, _info, tab) => {
-  if (tab.url) {
-    checkAndDisable(tab.url);
-  }
-});
-
-chrome.tabs.onActivated.addListener(async (activeInfo) => {
-  const tab = await chrome.tabs.get(activeInfo.tabId);
-  if (tab.url) {
-    checkAndDisable(tab.url);
-  }
-});
+chrome.tabs.onUpdated.addListener(checkTab);
+chrome.tabs.onActivated.addListener(checkTab);
+chrome.storage.onChanged.addListener(checkTab);
