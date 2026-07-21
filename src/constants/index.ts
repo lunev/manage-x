d4d279421f23833a8e0ceb197b4cc62fbf4ab22e
Olexandr L <lunev@aspentechlabs.com>
2026-07-21 19:50:39 +0200

    change meta

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const APP_NAME = 'ManageX';
+export const APP_NAME = 'ManageX – Extension Manager';
 export const STORAGE_KEY_ROOT = 'persist:localStorage';
 export const STORAGE_KEYS = {
   defaultExtensionsState: 'defaultExtState',
